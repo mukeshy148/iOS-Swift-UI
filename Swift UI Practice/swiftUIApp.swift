153ffefd89d2f7f6e231a1376b198bd74bc6ce35
Mukesh <mukesh148y@gmail.com>
2025-11-22 14:08:41 +0530

    Add files via upload
    1st Practice File

---
+++ Swift UI Practice/swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIApp.swift
+//  swiftUI
+//
+//  Created by Intern on 22/11/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
